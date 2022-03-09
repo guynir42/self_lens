271d5d4001d8b2084b8da102163f32241e047f78
Guy Nir <guyynir@gmail.com>
2022-03-08 16:03:37 -0800

    improve phase space plot

--- plots/make_plots.py
+++ plots/make_plots.py
@@ -46,16 +46,17 @@ sim.star_mass = 0.6
 sim.star_temp = 7000
 sim.declination = 0
 
-fig = plt.figure(num=2, figsize=[15, 10])
+fig = plt.figure(num=2, figsize=[18, 10])
 plt.clf()
+# ax = fig.add_axes([0.08, 0.08, 0.65, 0.87])
 ax = fig.add_axes([0.08, 0.08, 0.87, 0.87])
 
-thresholds = [0.01, 0.2, np.inf]
+thresholds = [0.03, 0.3, np.inf]
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
 # for WD lenses
-masses = np.array([0.3, 0.6, 1.0, 1.35])
+masses = np.array([0.3, 0.6, 1.0, 1.2])
 styles = ['*', 'v', 's', 'p']
 orbits = np.geomspace(0.0001, 1, 60)
 peak_mags = np.zeros((len(masses), len(orbits)))
@@ -77,8 +78,12 @@ for i, m in enumerate(masses):
         roche[i, j] = sim.roche_lobe / sim.star_size
 
         if peak_mags[i, j] > thresholds[t]:
+            vertical = 'top' if i % 2 == 0 else 'bottom'
+            horizontal = 'center' if i % 2 == 1 else 'left'
             prob = sim.best_prob_all_declinations_estimate(precision=thresholds[t], threshold=1)
-            ax.text(periods[i, j], peak_mags[i, j], f'P={prob:.4f}',
+            # text_str = f'P={prob:.2g}'
+            text_str = f'1/{int(np.round(1 / prob, -1))}'
+            ax.text(periods[i, j], peak_mags[i, j], text_str, va=vertical, ha=horizontal,
                     color='k', bbox=dict(facecolor=colors[i], alpha=0.8))
             t += 1
 
@@ -90,9 +95,9 @@ for i, m in enumerate(masses):
     ax.plot(periods[i, idx], peak_mags[i, idx], 'bo', fillstyle='none', markersize=12)
 
 # NS and BH lenses
-masses = np.array([2.0, 10.0, 30.0])
-colors = [np.ones(3) * c for c in [0.5, 0.3, 0.0]]
-styles = ['X', 'D', 'o']
+masses = np.array([1.5, 3, 10.0, 30.0])
+colors = [np.ones(3) * c for c in [0.6, 0.4, 0.2, 0.0]]
+styles = ['X', 'D', 'o', '^']
 peak_mags = np.zeros((len(masses), len(orbits)))
 periods = np.zeros(peak_mags.shape)
 prob_low = np.zeros(peak_mags.shape)
@@ -113,8 +118,10 @@ for i, m in enumerate(masses):
 
         if peak_mags[i, j] > thresholds[t]:
             prob = sim.best_prob_all_declinations_estimate(precision=thresholds[t], threshold=1)
-            ax.text(periods[i, j], peak_mags[i, j],
-                    f'P={prob:.2f}', color='k', bbox=dict(facecolor='w', alpha=0.8))
+            # text_str = f'P={prob:.2g}'
+            text_str = f'1/{int(np.round(1 / prob))}'
+            ax.text(periods[i, j], peak_mags[i, j], text_str, ha='right',
+                    color='k', bbox=dict(facecolor='w', alpha=0.8))
 
             t += 1
 
@@ -133,14 +140,14 @@ ax.set_yscale('log')
 
 ax.plot([0], [0], 'bo', markersize=12, fillstyle='none', label='Roche lobe overflow')
 
-ax.plot(np.array([0, 1e9]), np.ones(2) * thresholds[0], '--k', label=f'{int(thresholds[0]*1000)}mmag threshold', alpha=0.8)
-ax.plot(ax.get_xlim(), np.ones(2) * thresholds[1], '--k', label=f'{int(thresholds[1]*1000)}mmag threshold', alpha=0.5)
-
-ax_min = 1e-6
-ax_max = 100
+ax_min = 2e-3
+ax_max = 50
 
 ax.set_ylim(ax_min, ax_max)
-ax.set_xlim(3, 2e8)
+ax.set_xlim(0.45, 1.5e8)
+
+ax.plot(ax.get_xlim(), np.ones(2) * thresholds[0], '--k', label=f'{int(thresholds[0]*1000)}mmag threshold', alpha=0.8)
+ax.plot(ax.get_xlim(), np.ones(2) * thresholds[1], '--k', label=f'{int(thresholds[1]*1000)}mmag threshold', alpha=0.5)
 
 ax.plot(np.ones(2) * 60, ax.get_ylim(), ':k', alpha=0.3)
 ax.text(60*1.1, ax_max / 2, 'one minute')
@@ -151,12 +158,20 @@ ax.text(3600 * 24 * 1.1, ax_max / 2, 'one day')
 ax.plot(np.ones(2) * 3600 * 24 * 365, ax.get_ylim(), ':k', alpha=0.3)
 ax.text(3600 * 24 * 365 * 1.1, ax_max / 2, 'one year')
 
-ax.plot([0], [0], linestyle='none', label='"P=$\ldots$": geometric prob.')
+ax.plot([0], [0], linestyle='none', label='"1/10": Duty cycle')
 
-ax.legend(loc="lower right")
-
-
+# ax.legend(loc="lower right")
+# ax.set_position([0.08, 0.08, 0.64, 0.9])
+# ax.legend(loc="upper left", bbox_to_anchor=(1.04, 1))
+ax.legend()
 
 ## save the plot
 
 plt.savefig('plots/mag_vs_period.pdf')
+
+## show blackbody
+
+la = np.linspace(200, 1200, 1000)
+f = simulator.black_body(la, 5778)
+
+plt.plot(la, f)
